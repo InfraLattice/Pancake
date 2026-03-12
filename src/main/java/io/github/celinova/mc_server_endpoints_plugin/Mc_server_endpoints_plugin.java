df25b64fc03118e3290cc5b78da7a1fb05de090b
Celine Oades <celinova@celine-msi.localdomain>
2026-03-12 12:01:13 -0400

    Initial commit

---
+++ src/main/java/io/github/celinova/mc_server_endpoints_plugin/Mc_server_endpoints_plugin.java
@@ -0,0 +1,17 @@
+package io.github.celinova.mc_server_endpoints_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mc_server_endpoints_plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
